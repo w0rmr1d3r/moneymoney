c14aa1165eb27517c5a74e50e08d48be407b6ca1
Ramon <ramon.guimera@skyscanner.net>
2023-04-07 15:32:55 +0200

    add default moneys

---
+++ moneymoney/defaults.py
@@ -0,0 +1,6 @@
+from moneymoney.currency_codes import CurrencyCodes
+from moneymoney.money import Money
+
+ONE_EUR = Money(currency_code=CurrencyCodes.EUR)
+ONE_USD = Money(currency_code=CurrencyCodes.USD)
+ONE_GBP = Money(currency_code=CurrencyCodes.GBP)
