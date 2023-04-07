c0d87db74557aeadacc4655df90156fc18198ccc
Ramon <ramon.guimera@skyscanner.net>
2023-04-07 15:44:09 +0200

    update defaults order and add fixture

--- moneymoney/defaults.py
+++ moneymoney/defaults.py
@@ -2,5 +2,5 @@ from moneymoney.currency_codes import CurrencyCodes
 from moneymoney.money import Money
 
 ONE_EUR = Money(currency_code=CurrencyCodes.EUR)
-ONE_USD = Money(currency_code=CurrencyCodes.USD)
 ONE_GBP = Money(currency_code=CurrencyCodes.GBP)
+ONE_USD = Money(currency_code=CurrencyCodes.USD)
